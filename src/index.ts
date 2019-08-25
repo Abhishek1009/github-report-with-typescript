a240afbc88834705bb28fdca72490e3d9f6253a7
Abhishek1009 <ghoshabhishek1640@gmail.com>
2019-08-25 21:31:04 +0530

    added console input

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,13 @@ import { User } from "./User";
 import { Repo } from "./repo";
 import _ from "lodash";
 
+if (process.argv.length < 3) {
+    console.log('Please pass the username as an argument');
+}
+else {
+    let username = process.argv[2];
+}
+
 let userService= new GithubApiService();
 let userName="Abhishek1009";
 userService.getUserInfo(userName,(user:User)=>{
